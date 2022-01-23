afb70004ff08e6204903c0eb4167626b6954ee03
dennisn <denonjugush@gmail.com>
2022-01-23 15:07:55 +0200

    Added reorganised the file structure

---
+++ checkfiles.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin python
+
+"""This script processes all latex files to ensure diff comparisons
+are carried out in the correct files.
+
+"""
+import os
+import sys
+
+for file in os.listdir():
+    print(file)
+
+if __name__ == '__main__':
+    pass
