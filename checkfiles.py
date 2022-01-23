260dcc0877103434c85df1d096ae579cc603cbfe
dennisn <denonjugush@gmail.com>
2022-01-23 21:07:56 +0200

    Debugged all paths

--- checkfiles.py
+++ checkfiles.py
@@ -8,28 +8,51 @@ import os
 import sys
 import subprocess
 
-ignore_files = ['IEEEcsmag.cls', 'IEEEtran.bst', 'IEEEtran.cls', 'IEEEtranS.bst', 'README', 'main.bib', 'main.tex']
-ignore_folders = ['images', 'sections', 'tables']
+ignored_files = ['IEEEcsmag.cls', 'IEEEtran.bst', 'IEEEtran.cls', 'IEEEtranS.bst',
+    'README', 'main.bib', 'main.tex', 'main-diff.tex', 'mlopsv1.zip', 'mlopsv2.zip', '.DS_Store']
+folders = ['sections', 'tables']
 
 def test_function():
     """
     Test the execution of the tool
     """
+    print('>> {}'.format(test_function.__name__))
     cmd = './latexdiff/latexdiff latexdiff/example/example-draft.tex latexdiff/example/example-rev.tex > latexdiff/example/example-diff.tex'
     subprocess.run(cmd, shell=True)
+
+def is_file_dir_ignored(item):
+    if item in ignored_files or item in ignore_folders:
+        return True
+    else:
+        return False
+
+def command(dir, file):
+    """
+    Create a command to execute
+    """
+    tool = './latexdiff/latexdiff '
+    arg1 = 'manuscript/mlopsv1/{}/{} '.format(dir, file)
+    arg2 = 'manuscript/mlopsv2/{}/{} '.format(dir, file)
+    output = '> manuscript/mlopsdiff/{}/{}'.format(dir, file)
+    return tool + arg1 + arg2 + output
 
 def process_files():
     """
     Check all files and conduct differences.
     """
+    print('>> {}'.format(process_files.__name__))
+    v1 = './manuscript/mlopsv1/{}'
+    v2 = './manuscript/mlopsv2/{}'
+    for dir in os.listdir('./manuscript/mlopsv1'):
+        #print('{}:  {}:{}:{}'.format(dir, os.path.isdir(v1.format(dir)), os.path.isdir(v2.format(dir)), dir in folders))
+        if os.path.isdir(v1.format(dir)) and os.path.isdir(v2.format(dir)) and dir in folders:
+            for file in os.listdir('./manuscript/mlopsv2/{}'.format(dir)):
+                # diff the files and store them in folder
+                print('{}:{}'.format(dir, file))
+                cmd = command(dir, file)
+                subprocess.run(cmd, shell=True)
 
-    for item in os.listdir():
-        if os.isfile(item):
-            # check diff
-            subprocess.call(['./latexdiff/latexdiff', 'manuscript/mlopsv1/'.format(item), ''], shell=True) #using the call() function
-        if os.isdir(item):
-            for files in os.listdir(item):
-                subprocess.call(['./latexdiff/latexdiff', 'manuscript/mlopsv1/'.format(item), ''], shell=True) #using the call() function
 
 if __name__ == '__main__':
     test_function()
+    process_files()
