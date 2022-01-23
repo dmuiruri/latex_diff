7abc303db52c342ef949fd5c078da40f8d2029da
dennisn <denonjugush@gmail.com>
2022-01-23 17:28:22 +0200

    Added a test function

--- checkfiles.py
+++ checkfiles.py
@@ -6,9 +6,30 @@ are carried out in the correct files.
 """
 import os
 import sys
+import subprocess
 
-for file in os.listdir():
-    print(file)
+ignore_files = ['IEEEcsmag.cls', 'IEEEtran.bst', 'IEEEtran.cls', 'IEEEtranS.bst', 'README', 'main.bib', 'main.tex']
+ignore_folders = ['images', 'sections', 'tables']
+
+def test_function():
+    """
+    Test the execution of the tool
+    """
+    cmd = './latexdiff/latexdiff latexdiff/example/example-draft.tex latexdiff/example/example-rev.tex > latexdiff/example/example-diff.tex'
+    subprocess.run(cmd, shell=True)
+
+def process_files():
+    """
+    Check all files and conduct differences.
+    """
+
+    for item in os.listdir():
+        if os.isfile(item):
+            # check diff
+            subprocess.call(['./latexdiff/latexdiff', 'manuscript/mlopsv1/'.format(item), ''], shell=True) #using the call() function
+        if os.isdir(item):
+            for files in os.listdir(item):
+                subprocess.call(['./latexdiff/latexdiff', 'manuscript/mlopsv1/'.format(item), ''], shell=True) #using the call() function
 
 if __name__ == '__main__':
-    pass
+    test_function()
