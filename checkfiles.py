7d96c00589b71c86b8652f89aa108e97c0e846ae
dennisn <denonjugush@gmail.com>
2024-02-20 01:05:36 +0200

    Updated script to evaluate files in root directory

--- checkfiles.py
+++ checkfiles.py
@@ -7,12 +7,15 @@ are carried out in the correct files.
 import os
 import subprocess
 
-ignored_files = ['IEEEcsmag.cls', 'IEEEtran.bst', 'IEEEtran.cls', 'IEEEtranS.bst',
-    'README', 'main.bib', 'main.tex', 'main-diff.tex', 'mlopsv1.zip', 'mlopsv2.zip', '.DS_Store']
-folders = ['sections', 'tables']
+ignored_files = ['IEEEcsmag.cls', 'IEEEtran.bst', 'IEEEtran.cls', 'IEEEtranS.bst','README.md', 'main.bib',
+                 'main-diff.tex', 'mlopsv1.zip', 'mlopsv2.zip', '.DS_Store', 'references.bib', 'acmart.cls']
 
-old_ver = 'mlops_v2' #'mlopsv1'
-new_ver = 'mlops_v3'
+ignored_folders = ['images']
+
+folders = ['sections', 'tables', 'cases'] # folders to be processed
+
+old_ver = 'deployment_v1'
+new_ver = 'deployment_v2'
 
 def test_function():
     """
@@ -22,20 +25,14 @@ def test_function():
     cmd = './latexdiff/latexdiff latexdiff/example/example-draft.tex latexdiff/example/example-rev.tex > latexdiff/example/example-diff.tex'
     subprocess.run(cmd, shell=True)
 
-def is_file_dir_ignored(item):
-    if item in ignored_files or item in ignore_folders:
-        return True
-    else:
-        return False
-
 def command(dir, file):
     """
     Create a command to execute
     """
-    tool = './latexdiff/latexdiff '
-    arg1 = f'manuscript/{old_ver}/{dir}/{file}'#.format(dir, file)
-    arg2 = f' manuscript/{new_ver}/{dir}/{file}'#.format(dir, file)
-    output = f' > manuscript/mlopsdiff/{dir}/{file}'#.format(dir, file)
+    tool = "./latexdiff/latexdiff"
+    arg1 = f"manuscript/{old_ver}/{dir}/{file}"
+    arg2 = f"manuscript/{new_ver}/{dir}/{file}"
+    output = f"> manuscript/diff/{dir}/{file}"
     return tool + arg1 + arg2 + output
 
 def process_files():
@@ -43,14 +40,30 @@ def process_files():
     Check all files and conduct differences.
     """
     print('>> {}'.format(process_files.__name__))
-    v1 = f'./manuscript/{old_ver}/'
-    v2 = f'./manuscript/{new_ver}/'
-    for dir in os.listdir(f'./manuscript/{old_ver}'):
-        if os.path.isdir(v1 + dir) and os.path.isdir(v2 + dir) and dir in folders:
-            for file in os.listdir(f'./manuscript/{new_ver}/' + dir):
-                print('{}:{}'.format(dir, file))
-                cmd = command(dir, file)
-                subprocess.run(cmd, shell=True)
+    v1 = f"./manuscript/{old_ver}/"
+    v2 = f"./manuscript/{new_ver}/"
+    tool = "./latexdiff/latexdiff "
+    # first process files in the root folder then walk through the directories
+    for f in os.listdir(f'./manuscript/{old_ver}'):
+        fp = os.path.join(f'./manuscript/{old_ver}', f)
+        if os.path.isfile(fp) and f not in ignored_files:
+                print(f'>> {v1}/{f}')
+                arg1 = f"{v1}/{f} "
+                arg2 = f"{v2}/{f} "
+                output = f"> manuscript/diff/{f} "
+                subprocess.run(tool + arg1 + arg2 + output, shell=True)
+        else:
+            dirpath = os.path.join(v1, f)
+            if os.path.isdir(f"{dirpath}"):
+                if os.path.isdir(v1 + f) and os.path.isdir(v2 + f) and f in folders:
+                    print(os.path.isdir(v1 + f))
+                    for file in os.listdir(v2 + f):
+                        print(f"{v2}{f}/{file}")
+                        tool = "./latexdiff/latexdiff "
+                        arg1 = f"{v1}{f}/{file} "
+                        arg2 = f"{v2}{f}/{file} "
+                        output = f"> manuscript/diff/{f}/{file}"
+                        subprocess.run(f"mkdir -p manuscript/diff/{f} && " + tool + arg1 + arg2 + output, shell=True)
 
 def process_standalone_files():
         """
@@ -66,6 +79,6 @@ def process_standalone_files():
             subprocess.run(cmd, shell=True)
 
 if __name__ == '__main__':
-    test_function()
+    # test_function()
     process_files()
-    process_standalone_files()
+    # process_standalone_files()
---
+++ manuscript/deployment_v1/README.md
@@ -0,0 +1,2 @@
+# deployment_patterns_alpha
+A backup for the manuscript (deployment patterns and inference architectures)
